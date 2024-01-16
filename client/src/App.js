e1382b75a95447c4bdf2081bb1907ca5cdce49a7
jihyean <d9rmlatte@gmail.com>
2024-01-16 17:05:33 +0900

    [037] reactstrap Dropdown사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBreadcrumbs from './PART2/R036_ReactstrapBreadcrumbs'
+import ReactstrapDropdown from './PART2/R037_ReactstrapDropdown'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapBreadcrumbs />
+      <ReactstrapDropdown />
     </div>
   );
 }
