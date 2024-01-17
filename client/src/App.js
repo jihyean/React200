5d9eda378fb1e856f853027fe5a334db0334a52d
jihyean <d9rmlatte@gmail.com>
2024-01-17 14:41:47 +0900

    [050] reactstrap Pagination 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapNavbar from './PART2/R049_ReactstrapNavbar'
+import ReactstrapPagination from './PART2/R050_ReactstrapPagination'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapNavbar />
+      <ReactstrapPagination />
     </div>
   );
 }
