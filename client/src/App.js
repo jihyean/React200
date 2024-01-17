d6f1336630b1f5d698701226988fe1ca6b063d56
jihyean <d9rmlatte@gmail.com>
2024-01-17 14:40:41 +0900

    [049] reactstrap Navbar 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapModal from './PART2/R048_ReactstrapModal'
+import ReactstrapNavbar from './PART2/R049_ReactstrapNavbar'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapModal />
+      <ReactstrapNavbar />
     </div>
   );
 }
