1b526231ea2f1e0d88ad8f58d7edcc790b36b1c9
jihyean <d9rmlatte@gmail.com>
2024-01-17 14:20:17 +0900

    [043] reactstrap Fade 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapCollapse from './PART2/R042_ReactstrapCollapse'
+import ReactstrapFade from './PART2/R043_ReactstrapFade'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapCollapse />
+      <ReactstrapFade />
     </div>
   );
 }
