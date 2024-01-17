6e9d8a4b9b29040c8c5f00f1b04b8a6bc5992837
jihyean <d9rmlatte@gmail.com>
2024-01-17 14:13:24 +0900

    [040] reactstrap Card 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBottons from './PART2/R039_ReactstrapBottons'
+import ReactstrapCard from './PART2/R040_ReactstrapCard'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapBottons />
+      <ReactstrapCard />
     </div>
   );
 }
