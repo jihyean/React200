90f2cab3fc1fd9f465fecf172a61b43fe09dd2b9
jihyean <d9rmlatte@gmail.com>
2024-01-18 16:52:55 +0900

    [054] reactstrap Table 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapSpinner from './PART2/R053_ReactstrapSpinner'
+import ReactstrapTable from './PART2/R054_ReactstrapTable'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapSpinner />
+      <ReactstrapTable />
     </div>
   );
 }
