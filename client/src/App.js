e30dc06ea1ab90c082c099b7bcb8920563864896
jihyean <d9rmlatte@gmail.com>
2024-01-18 16:47:25 +0900

    [052] reactstrap Progress 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapPopover from './PART2/R051_ReactstrapPopover'
+import ReactstrapProgress from './PART2/R052_ReactstrapProgress'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapPopover />
+      <ReactstrapProgress />
     </div>
   );
 }
