acfac2f40e526699a695384f6e4149e74b39dd79
jihyean <d9rmlatte@gmail.com>
2024-01-22 11:23:20 +0900

    [060] fetch post 호출하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import FetchGet from './PART2/R059_FetchGet'
+import FetchPost from './PART2/R060_FetchPost'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <FetchGet />
+      <FetchPost />
     </div>
   );
 }
