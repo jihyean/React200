0efd0f5dd4c77f443be3fa3abb0aeb0119733972
jihyean <d9rmlatte@gmail.com>
2024-01-22 11:26:00 +0900

    [061] axios get 호출하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import FetchPost from './PART2/R060_FetchPost'
+import AxiosGet from './PART2/R061_AxiosGet'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <FetchPost />
+      <AxiosGet />
     </div>
   );
 }
