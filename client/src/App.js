f68c8a88eedbe3ad608619e27aa139d6aad615ac
jihyean <d9rmlatte@gmail.com>
2024-01-25 16:19:19 +0900

    [075] 하이오더 컴포넌트 구현하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
-import ReactCurrying from './PART3/R074_ReactCurrying'
+import ReactHoc1 from './PART3/R075_ReactHoc1'
 import 'bootstrap/dist/css/bootstrap.css'
 
-// 커링(Currying)은 함수를 재사용성을 높이기 위해 함수 자체를 return한다
-// 함수 하나만 사용할 때는 필요한 파라미터를 한 번에 넣어야 한다
-// 커링을 사용하면 함수를 분리하여 필요한 파라미터를 나눠서 넣을 수 있다
+// 함수 자체를 인자로 받거나 반환하는 함수를 '고차 함수'라고 함
+// 컴포넌트를 인자로 받거나 반환하는 함수를 '고차 컴포넌트'라고 함
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactCurrying />
+      <ReactHoc1 />
     </div>
   );
 }
